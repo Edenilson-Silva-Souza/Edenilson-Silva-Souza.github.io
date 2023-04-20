662917d14907265a5021d6e5c0552b08a8569116
Edenilson-Silva-Souza <edenilsom717@gmail.com>
2023-04-20 20:28:32 -0300

    mudei o design para o PC - Mudei também o texto de apresentação do mobile e PC

--- javascript/tools.js
+++ javascript/tools.js
@@ -2,25 +2,59 @@ var size=0, milissegundos=0, pointer2=0;
 var mensage="";
 var mensagens=[];
 window.onload= main();
+var tagp="";
 function main(){
 	mensage = document.getElementById("area_mensagens").innerHTML;
 	
-	size = mensage.length;
+	quantP = contP();
+	size = mensage.length+quantP;
+	
 	pointer2=0;
 	document.getElementById("area_mensagens").innerHTML="";
 	dialog();
 }
 
+function contP(){
+	let salt=0;
+	cont = 0 ;
+	while(mensage.indexOf("<p>",salt)!=-1){
+		salt =  mensage.indexOf("<p>",salt+1);
+		cont++;
+		if(salt==-1){
+			break;
+		}
+	}
+	return cont+3;
+}
+
 function dialog(){	
 	milissegundos--;
 	if(milissegundos<=0){
-	document.getElementById("area_mensagens").innerHTML+=mensage[pointer2];
+		
+	if(mensage[pointer2]=="<"){
+		tagp+="<";
+		
+	}
+	else if(tagp=="<"){
+		tagp+="p";
+	}
+	else if(tagp=="<p"){
+		tagp+=">";
+	}
+	else if(tagp=="<p>"){
+		document.getElementById("area_mensagens").innerHTML+="<p>";
+		tagp="";
+	}
+	else{
+		document.getElementById("area_mensagens").innerHTML+=mensage[pointer2];
+	}
+	
 	
 	
 	pointer2++;
 	}
 	if(milissegundos<=0){
-		milissegundos=4;
+		milissegundos=0;
 	}
 	if(document.getElementById("area_mensagens").innerHTML.length==size){
 		return false;
